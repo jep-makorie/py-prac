cf3c5532b62530d79e425dbe27441eba3ffbeb6c
jep-makorie <preciousmarjorie412@gmail.com>
2026-09-03 23:58:21 -0400

    Further function practice

--- dayfour.py
+++ dayfour.py
@@ -57,4 +57,36 @@ def check_pin(entered_pin, correct_pin):
 
 message=check_pin("4721","4821")
 print(message)
+
+
+def free_shipping(total, member):
+
+  if total>=30 and member=="yes":
+    return True
+  elif total>=50 and member=="no":
+    return True
+  return False
+
+total1=float(input("What is your total?: "))
+membership= input("Are you a member?: ")
+membership=membership.strip().lower()
+
+if free_shipping(total1, membership):
+  print(f"${total1} order. Free shipping applied")
+else:
+  print(f"${total1} order. Shipping fee required")
 """
+def movie_checkout(price, age, student):
+  if age<=11:
+    return price*0.5
+  elif student=="yes":
+    return price*0.8
+  return price
+
+ticket_price=float(input("What is the original price of the ticket?: "))
+age=int(input("How old are you?: "))
+is_student=input("Are you are student?: ")
+is_student=is_student.lower().strip()
+final_price = movie_checkout(ticket_price, age, is_student)
+
+print(f"Final price: ${final_price}")
